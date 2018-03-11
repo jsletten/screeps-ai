d7cc9fbcdd2820ccbb91e8b7044b3fdee5a40707
Sletten <jsletten@plexemp.local>
2018-03-11 17:25:00 -0400

    Basic Worker return to base with energy

--- role.basicWorker.js
+++ role.basicWorker.js
@@ -1,24 +1,34 @@
 var roleBasicWorker = {
     
     spawnCreep: function() {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'basicWorker'});
+            var newName = Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], undefined,{
+                memory: {role: 'basicWorker'}
+            }); 
             //Game.creeps[newName].memory.containerID = containerID;
             console.log('Spawning new BasicWorker: ' + newName);
             return newName;
     },
     
     /** @param {Creep} creep **/
-    run: function(creep, remote = false) {
+    run: function(creep) {
 
-
-        var source = creep.pos.findClosestByRange(FIND_SOURCES);
+        if(_.sum(creep.carry) < creep.carryCapacity) {
+            var source = creep.pos.findClosestByRange(FIND_SOURCES);
         
-        if(source) {
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
+            if(source) {
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
+            }  
+        }
+        else {
+            var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => { 
+                return (structure.structureType == STRUCTURE_SPAWN)}});            
+                
+            if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
             }
-        }     
-            
+        }
     }
 };
 
