afebb95b03bb7f2b190cec011b8dc567f9188182
Sletten <jsletten@plexemp.local>
2018-03-12 09:47:44 -0400

    Make Hauler container aware

--- role.containerHauler.js
+++ role.containerHauler.js
@@ -1,7 +1,7 @@
 var roleContainerHauler = {
     
-    spawnCreep: function() {
-            var newName = Game.spawns['Spawn1'].createCreep([CARRY,CARRY,MOVE,MOVE], undefined, {role: 'containerHauler'});
+    spawnCreep: function(containerID) {
+            var newName = Game.spawns['Spawn1'].createCreep([CARRY,CARRY,MOVE,MOVE], undefined, {role: 'containerHauler', containerID: containerID});
             console.log('Spawning new ContainerHauler: ' + newName);
             return newName;
     },
@@ -10,7 +10,7 @@ var roleContainerHauler = {
     run: function(creep) {
         if(_.sum(creep.carry)  == 0) {
 
-           var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => ((structure.structureType == STRUCTURE_CONTAINER ) && (structure.store[RESOURCE_ENERGY] > 0)) });   
+            var target = Game.getObjectById(creep.memory.containerID);
                     
             if(creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
@@ -18,7 +18,7 @@ var roleContainerHauler = {
             
         }
         else {
-            
+            //TODO: Make this goto Storage first, if that doesn't exist then fall back to Spawn/Extension/Tower for low level rooms.
             var target = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {filter: (structure) => { 
             return (structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_TOWER ) && ((structure.energy < structure.energyCapacity)&&(structure.energy < 800))}});            
             
