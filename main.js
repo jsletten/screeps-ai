3e1d65e4bdd08448d0910def57b4fd58782fb1d7
Sletten <jsletten@plexemp.local>
2018-03-14 23:05:01 -0400

    debugging

--- main.js
+++ main.js
@@ -2,6 +2,20 @@
 
 // import modules
 require('prototype.game'); //This must be first
+
+Game.prototype.roles = null;
+
+Game.roles = {
+    basicWorker: require('role.basicWorker'),
+    containerHarvester: require('role.containerHarvester'),
+    containerHauler: require('role.containerHauler'),
+    upgrader: require('role.upgrader'),
+    builder: require('role.builder'),
+    cleaner: require('role.cleaner'),
+    claimer: require('role.claimer'),
+    attacker: require('role.attacker')
+};
+
 require('prototype.creep');
 var roleTower = require('role.tower');
 
