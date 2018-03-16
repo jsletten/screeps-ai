e8b48e9955125b5d6d085e741781e7609ba14053
Sletten <jsletten@plexemp.local>
2018-03-15 20:18:57 -0400

    Make Hauler room aware

--- role.containerHauler.js
+++ role.containerHauler.js
@@ -1,20 +1,20 @@
 module.exports = {
-    spawnCreep: function(containerID, emergencySpawn) 
+    spawnCreep: function(containerID, emergencySpawn, homeRoom = 'E32N13') 
     {
         var spawn = Game.spawns['Spawn1'];
         if((spawn.room.energyCapacityAvailable >= 450) && !emergencySpawn)
         {
-            var result = spawn.createCreep([CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: 'containerHauler', containerID: containerID});
+            var result = spawn.createCreep([CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE], undefined, {role: 'containerHauler', containerID: containerID, homeRoom: homeRoom});
             console.log('Spawning new ContainerHauler(large): ' + result);
         }
         else if((spawn.room.energyCapacityAvailable >= 300) && !emergencySpawn)
         {
-            var result = spawn.createCreep([CARRY,CARRY,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'containerHauler', containerID: containerID});
+            var result = spawn.createCreep([CARRY,CARRY,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'containerHauler', containerID: containerID, homeRoom: homeRoom});
             console.log('Spawning new ContainerHauler(med): ' + result);
         }
         else
         {
-            var result = spawn.createCreep([CARRY,CARRY,MOVE,MOVE], undefined, {role: 'containerHauler', containerID: containerID});
+            var result = spawn.createCreep([CARRY,CARRY,MOVE,MOVE], undefined, {role: 'containerHauler', containerID: containerID, homeRoom: homeRoom});
             console.log('Spawning new ContainerHauler(small): ' + result);
         }
         
@@ -39,20 +39,29 @@ module.exports = {
             }
         }
         else {
-            
-            var target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: (structure) => { 
-            return (structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_TOWER ) && ((structure.energy < structure.energyCapacity)&&(structure.energy < 800))}});            
-            
-            //If Spawn / Towers are full put remainder in storage
-            if(target === 'undefined' || target === null)
+            if(creep.room.name != creep.memory.homeRoom)
             {
-                target = creep.room.storage;
-            }    
+                // find exit to target room
+                var exit = creep.room.findExitTo(creep.memory.homeRoom);
+                // move to exit
+                creep.moveTo(creep.pos.findClosestByRange(exit));
+            }
+            else
+            {
+                var target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: (structure) => { 
+                return (structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_TOWER ) && ((structure.energy < structure.energyCapacity)&&(structure.energy < 800))}});            
+                
+                //If Spawn / Towers are full put remainder in storage
+                if(target === 'undefined' || target === null)
+                {
+                    target = creep.room.storage;
+                }    
 
-            if(target)
-            {
-                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ff0000'}});
+                if(target)
+                {
+                    if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target, {visualizePathStyle: {stroke: '#ff0000'}});
+                    }
                 }
             }
         }
