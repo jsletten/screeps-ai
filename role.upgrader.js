b2774bdf52851293c006e82e9e9c7af8330bf74e
Sletten <jsletten@plexemp.local>
2018-03-25 22:49:44 -0400

    Make upgrader spawn based on available energy

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,37 +1,30 @@
 module.exports = {
     spawnCreep: function(spawn) {
-        if(spawn.room.energyCapacityAvailable >= 1200)
+
+        let body = [];
+        let maxEnergy = spawn.room.energyCapacityAvailable;
+        let numberOfParts;
+
+        //1x WORK - 1x CARRY - 1X MOVE
+        numberOfParts = Math.floor(maxEnergy / 200) * 3;
+        numberOfParts = Math.min(numberOfParts, 50); // creeps can't exceed 50
+        for (let i = 0; i < numberOfParts/3; i++)
         {
-            var newName = spawn.createCreep([WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'upgrader'});
-            console.log('Spawning new upgrader(xx-large): ' + newName);  
+            body.push(WORK);
+            body.push(CARRY);
+            body.push(MOVE);
         }
-        else if(spawn.room.energyCapacityAvailable >= 700)
-        {
-            var newName = spawn.createCreep([WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE], undefined, {role: 'upgrader'});
-            console.log('Spawning new upgrader(x-large): ' + newName);  
-        }
-        else if(spawn.room.energyCapacityAvailable >= 550)
-        {
-            var newName = spawn.createCreep([WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader'});
-            console.log('Spawning new upgrader(large): ' + newName);  
-        }
-        else if(spawn.room.energyCapacityAvailable >= 350)
-        {
-            var newName = spawn.createCreep([WORK,WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader'});
-            console.log('Spawning new upgrader(med): ' + newName);  
-        }
-        else
-        {
-            var newName = spawn.createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
-            console.log('Spawning new upgrader(small): ' + newName);
-        }
+
+        let newName = spawn.createCreep(body, undefined, {role: 'upgrader'});
+        console.log('Spawning new upgrader(' + numberOfParts + '): ' + newName);
+
         return;
     },
     /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.carry.energy == 0) 
         {    
-            var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => { 
+            let target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => { 
                 return ((structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 0)}});            
             
             if(!target)
