1a0c387a1a15b3905173a2766c1b1b5f1662edb9
Sletten <jsletten@plexemp.local>
2018-04-06 13:50:37 -0400

    enable logging

--- prototype.creep.js
+++ prototype.creep.js
@@ -3,6 +3,17 @@ var Globals = require('globals');
 Creep.prototype.runRole =
     function () 
     {
-        //console.log('name: ' + this.name + ' role: ' +this.memory.role);
+        console.log('name: ' + this.name + ' role: ' + this.memory.role);
         Globals.roles[this.memory.role].run(this);
     };
+
+Object.defineProperty(Creep.prototype, 'isFull', {
+    get: function() {
+        if (!this._isFull) {
+            this._isFull = _.sum(this.carry) === this.carryCapacity;
+        }
+        return this._isFull;
+    },
+    enumerable: false,
+    configurable: true
+});
