216810c9513197accee5b78bc453452a91d73259
Sletten <jsletten@plexemp.local>
2018-04-07 08:59:18 -0400

    clean up roles for active creeps

--- prototype.creep.js
+++ prototype.creep.js
@@ -3,6 +3,10 @@ var Globals = require('globals');
 Creep.prototype.runRole =
     function () 
     {
+        if(this.memory.role == 'containerHauler')
+        {
+            this.memory.role = 'containerTransport';
+        }
         console.log('name: ' + this.name + ' role: ' + this.memory.role);
         Globals.roles[this.memory.role].run(this);
     };
