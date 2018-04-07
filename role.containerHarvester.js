f6af5381675cee08d288930ec3a1ab9c8cd5414e
Sletten <jsletten@plexemp.local>
2018-04-07 08:53:50 -0400

    renamed wrong file

---
+++ role.containerHarvester.js
@@ -0,0 +1,139 @@
+module.exports = {
+    spawnCreep: function(spawn, targetIndex, emergencySpawn, targetRoom = 'E32N13', harvestEnergy = true) 
+    {
+        var body = [];
+        var maxEnergy = spawn.room.energyCapacityAvailable - 50;
+        var numberOfParts = Math.floor(maxEnergy / 250) * 3;
+        var role = 'containerHarvester';
+
+        if(emergencySpawn)
+        {
+            numberOfParts = 3;
+        }
+
+        if(harvestEnergy)
+        {
+            //Energy Harvesters don't need more then 6 WORK parts to keep up with node respawn.
+            numberOfParts = Math.min(numberOfParts, 9);
+        }
+        else
+        {
+            role = 'mineralHarvester';
+            // make sure the creep is not too big )
+            numberOfParts = Math.min(numberOfParts, 48);
+        }        
+
+        //1 MOVE part for every 2 WORK parts
+        for (let i = 0; i < ((numberOfParts/3)*2); i++) {
+            body.push(WORK);
+        }
+        for (let i = 0; i < (numberOfParts/3); i++) {
+            body.push(MOVE);
+        }
+
+        //Add the single CARRY part
+        body.push(CARRY);
+
+        var newName = spawn.createCreep(body, undefined, {role: role, targetIndex: targetIndex, targetRoom: targetRoom});
+        console.log('Spawning new ' + role + '(' + numberOfParts + ') target: ' + targetRoom + '(' + targetIndex + '): ' + newName);     
+        
+        //let memory = {role: role, targetIndex: targetIndex, targetRoom: targetRoom};
+
+        //return {body: body, memory: memory};;
+    },
+    
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if (creep.room.name != creep.memory.targetRoom)
+        {
+            // find exit to target room
+            var exit = creep.room.findExitTo(creep.memory.targetRoom);
+            // move to exit
+            creep.moveTo(creep.pos.findClosestByRange(exit));
+        }
+        else
+        {
+            var target;
+            var container;
+
+            if(creep.memory.role == 'containerHarvester')
+            {
+                target = creep.room.sources[creep.memory.targetIndex];
+            }
+            else
+            {
+                target = creep.room.find(FIND_MINERALS)[creep.memory.targetIndex];
+            }
+
+            if(creep.memory.containerID)
+            {
+                container = Game.getObjectById(creep.memory.containerID);
+            }
+            else
+            {
+                container = target.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => { return (structure.structureType == STRUCTURE_CONTAINER) && (structure.pos.inRangeTo(target, 1)) }});
+                if(container) 
+                {
+                    creep.memory.containerID = container.id;
+                }
+            }
+            
+            if(container)
+            {
+                if(creep.pos.isEqualTo(container.pos))
+                {
+                    if(creep.carry[RESOURCE_ENERGY] > 0 && container.hits < container.hitsMax)
+                    {   
+                        creep.say('Reapiring!');
+                        creep.repair(container);
+                    }
+                    else
+                    {
+                        creep.harvest(target);
+
+                        for(const resourceType in creep.carry) {
+                            creep.transfer(container, resourceType);
+                        }
+
+                        let links = creep.pos.findInRange(FIND_MY_STRUCTURES, 1, {filter: (structure) => { 
+                            return ((structure.structureType == STRUCTURE_LINK) && (structure.energy < structure.energyCapacity)) }});
+                        
+                        if(links.length > 0)
+                        {
+                            creep.transfer(links[0], RESOURCE_ENERGY);
+                        }
+                    }
+                }
+                else
+                {
+                    creep.moveTo(container);
+                }
+            }
+            else
+            {
+                var site = target.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES, {filter: (structure) => { return (structure.structureType == STRUCTURE_CONTAINER) && (structure.pos.inRangeTo(target, 1)) }});
+                if(!site)
+                {
+                    //No Container && No Construction Site
+                    if(creep.pos.inRangeTo(target, 1))
+                    {
+                        creep.room.createConstructionSite(creep.pos, STRUCTURE_CONTAINER);
+                    }
+                    else
+                    {
+                        creep.moveTo(target);
+                    }
+                }
+                else
+                {
+                    if(creep.harvest(target) == ERR_NOT_IN_RANGE) 
+                    {
+                        creep.moveTo(site);
+                    }
+                    creep.say('build');
+                    creep.build(site)
+                }            
+            }
+        }
+    }
+};
--- role.containerTransport.js
+++ role.containerTransport.js
@@ -1,139 +1,129 @@
 module.exports = {
-    spawnCreep: function(spawn, targetIndex, emergencySpawn, targetRoom = 'E32N13', harvestEnergy = true) 
+    spawnCreep: function(spawn, containerID, emergencySpawn, homeRoom = 'E32N13') 
     {
-        var body = [];
-        var maxEnergy = spawn.room.energyCapacityAvailable - 50;
-        var numberOfParts = Math.floor(maxEnergy / 250) * 3;
-        var role = 'containerHarvester';
+        let target = Game.getObjectById(containerID);
+        let memory = {role: 'containerTransport', containerID: containerID, homeRoom: homeRoom};
+        let body = [];
+        let maxEnergy
+        let numberOfParts;
 
         if(emergencySpawn)
         {
-            numberOfParts = 3;
-        }
-
-        if(harvestEnergy)
-        {
-            //Energy Harvesters don't need more then 6 WORK parts to keep up with node respawn.
-            numberOfParts = Math.min(numberOfParts, 9);
+            maxEnergy = 150;
         }
         else
         {
-            role = 'mineralHarvester';
-            // make sure the creep is not too big )
-            numberOfParts = Math.min(numberOfParts, 48);
-        }        
+            maxEnergy = spawn.room.energyCapacityAvailable;
+        }
 
-        //1 MOVE part for every 2 WORK parts
+        if(spawn.room == target.room)
+        {
+            maxEnergy = Math.min(maxEnergy, 450);
+        }
+        else
+        {
+            maxEnergy = Math.min(maxEnergy, 900);
+        }
+
+        numberOfParts = Math.floor(maxEnergy / 150) * 3;
+
+        //1 MOVE part for every 2 CARRY parts
         for (let i = 0; i < ((numberOfParts/3)*2); i++) {
-            body.push(WORK);
+            body.push(CARRY);
         }
         for (let i = 0; i < (numberOfParts/3); i++) {
             body.push(MOVE);
         }
 
-        //Add the single CARRY part
-        body.push(CARRY);
-
-        var newName = spawn.createCreep(body, undefined, {role: role, targetIndex: targetIndex, targetRoom: targetRoom});
-        console.log('Spawning new ' + role + '(' + numberOfParts + ') target: ' + targetRoom + '(' + targetIndex + '): ' + newName);     
         
-        //let memory = {role: role, targetIndex: targetIndex, targetRoom: targetRoom};
-
-        //return {body: body, memory: memory};;
+        let newName = spawn.spawnCreep(body, 'CT-' + Game.time, {memory: memory});
+        console.log('Spawning new ' + memory.role + '(' + numberOfParts + '): containerID(' + containerID  + ') homeRoom(' + homeRoom + '): ' + newName);
+        
+        //return {body: body, memory: memory};
     },
     
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (creep.room.name != creep.memory.targetRoom)
-        {
-            // find exit to target room
-            var exit = creep.room.findExitTo(creep.memory.targetRoom);
-            // move to exit
-            creep.moveTo(creep.pos.findClosestByRange(exit));
-        }
-        else
-        {
-            var target;
-            var container;
-
-            if(creep.memory.role == 'containerHarvester')
+        if(_.sum(creep.carry)  == 0) {
+            if(creep.room.name == creep.memory.homeRoom && creep.ticksToLive < 100)
             {
-                target = creep.room.sources[creep.memory.targetIndex];
-            }
-            else
-            {
-                target = creep.room.find(FIND_MINERALS)[creep.memory.targetIndex];
+                creep.suicide();
             }
 
-            if(creep.memory.containerID)
+            let target = Game.getObjectById(creep.memory.containerID);
+                 
+            if(_.sum(target.store) < 100)
             {
-                container = Game.getObjectById(creep.memory.containerID);
-            }
-            else
-            {
-                container = target.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => { return (structure.structureType == STRUCTURE_CONTAINER) && (structure.pos.inRangeTo(target, 1)) }});
-                if(container) 
-                {
-                    creep.memory.containerID = container.id;
-                }
-            }
-            
-            if(container)
-            {
-                if(creep.pos.isEqualTo(container.pos))
-                {
-                    if(creep.carry[RESOURCE_ENERGY] > 0 && container.hits < container.hitsMax)
-                    {   
-                        creep.say('Reapiring!');
-                        creep.repair(container);
-                    }
-                    else
-                    {
-                        creep.harvest(target);
+                //Act as Cleaner
+                target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
 
-                        for(const resourceType in creep.carry) {
-                            creep.transfer(container, resourceType);
-                        }
-
-                        let links = creep.pos.findInRange(FIND_MY_STRUCTURES, 1, {filter: (structure) => { 
-                            return ((structure.structureType == STRUCTURE_LINK) && (structure.energy < structure.energyCapacity)) }});
-                        
-                        if(links.length > 0)
-                        {
-                            creep.transfer(links[0], RESOURCE_ENERGY);
-                        }
-                    }
-                }
-                else
-                {
-                    creep.moveTo(container);
-                }
-            }
-            else
-            {
-                var site = target.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES, {filter: (structure) => { return (structure.structureType == STRUCTURE_CONTAINER) && (structure.pos.inRangeTo(target, 1)) }});
-                if(!site)
-                {
-                    //No Container && No Construction Site
-                    if(creep.pos.inRangeTo(target, 1))
-                    {
-                        creep.room.createConstructionSite(creep.pos, STRUCTURE_CONTAINER);
-                    }
-                    else
-                    {
+                if(target) {
+                    creep.say('Resource!');
+                    if(creep.pickup(target) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(target);
                     }
                 }
+                else 
+                {
+                    target = creep.pos.findClosestByPath(FIND_TOMBSTONES, {filter: (tombstone) => { 
+                        return (_.sum(tombstone.store) > 0)}}); 
+                
+                    if(target)
+                    {
+                        for(resourceType in target.store) 
+                        {
+                            if(creep.withdraw(target, resourceType) == ERR_NOT_IN_RANGE) {
+                                creep.moveTo(target);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                //Get resources from container
+                for(resourceType in target.store) 
+                {
+                    if(creep.withdraw(target, resourceType) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                }
+            }
+        }
+        else {
+            if(creep.room.name != creep.memory.homeRoom)
+            {
+                // find exit to target room
+                let exit = creep.room.findExitTo(creep.memory.homeRoom);
+                // move to exit
+                creep.moveTo(creep.pos.findClosestByRange(exit));
+            }
+            else
+            {
+                let target;
+                
+                if(_.sum(creep.carry) == creep.carry[RESOURCE_ENERGY]) //Only energy so deposit anywhere
+                {
+                    target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: (structure) => { 
+                        return ((structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_TOWER || structure.structureType == STRUCTURE_LINK) && ((structure.energy < structure.energyCapacity)&&(structure.energy < 800))
+                            || (structure.structureType == STRUCTURE_STORAGE))}}); 
+                }
                 else
                 {
-                    if(creep.harvest(target) == ERR_NOT_IN_RANGE) 
+                    target = creep.room.storage; //Carrying resources other then energy so must deposit to storage
+                }
+            
+                if(target)
+                {
+                    for(resourceType in creep.carry) 
                     {
-                        creep.moveTo(site);
+                        if(creep.transfer(target, resourceType) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(target);
+                        }
                     }
-                    creep.say('build');
-                    creep.build(site)
-                }            
+                }
             }
         }
     }
 };
+
