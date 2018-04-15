ad14a10a3bb948a0f14ef2da11d80c891d3775ae
Sletten <jsletten@plexemp.local>
2018-04-14 22:51:42 -0400

    remove temporary memory cleanup code

--- prototype.creep.js
+++ prototype.creep.js
@@ -4,10 +4,10 @@ Creep.prototype.runRole =
     function () 
     {
         // Use this code when renaming roles of active creeps
-        if(this.memory.role == 'containerTransport')
-        {
-            this.memory.targetID = this.memory.targetID || this.memory.containerID;
-        }
+        // if(this.memory.role == 'containerTransport')
+        // {
+        //     this.memory.targetID = this.memory.targetID || this.memory.containerID;
+        // }
         //console.log('name: ' + this.name + ' role: ' + this.memory.role);
         Globals.roles[this.memory.role].run(this);
     };
