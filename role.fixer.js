c38183dc666fd84e07c7496a90115ba5c855958c
Sletten <jsletten@plexemp.local>
2018-04-17 09:19:00 -0400

    Don't build enemy buildings

--- role.fixer.js
+++ role.fixer.js
@@ -37,9 +37,9 @@ module.exports = {
             }
             else
             {
-                let  targets = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
+                let  targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 
-                if(targets.length > 0) {
+                if(targets.length > 0 && targets[0].owner  == undefined) {
                     if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffff00'}});
                     }
