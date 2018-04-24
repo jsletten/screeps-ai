09310ceed3c08fe1478f544dff11ad202e6d5894
Sletten <jsletten@plexemp.local>
2018-04-24 15:02:36 -0400

    make sure objects exist before using them

--- role.settler.js
+++ role.settler.js
@@ -43,20 +43,23 @@ module.exports = {
             let targets = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
             let source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
 
-            if((_.sum(creep.carry)  == 0) || (_.sum(creep.carry) < creep.carryCapacity && creep.pos.inRangeTo(source, 1)))
+            if(targets && source)
             {
-                if(creep.harvest(source) == ERR_NOT_IN_RANGE) 
+                if((_.sum(creep.carry)  == 0) || (_.sum(creep.carry) < creep.carryCapacity && creep.pos.inRangeTo(source, 1)))
                 {
-                    creep.moveTo(source);
-                }
-            }  
-            else
-            {
-                if(targets.length > 0) 
-                {
-                    if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) 
+                    if(creep.harvest(source) == ERR_NOT_IN_RANGE) 
                     {
-                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffff00'}});
+                        creep.moveTo(source);
+                    }
+                }  
+                else
+                {
+                    if(targets.length > 0) 
+                    {
+                        if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) 
+                        {
+                            creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffff00'}});
+                        }
                     }
                 }
             }
