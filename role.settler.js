e0a934574988340d0a2a159fd6facee5c6f5f19a
Jeremy Sletten <jsletten@enprecis.local>
2019-05-19 11:56:23 -0400

    allow settler to operate without flag

--- role.settler.js
+++ role.settler.js
@@ -40,38 +40,35 @@ module.exports = {
         }
         else
         {   
-            if(Game.flags.settlerFlag)
+            if(Game.flags.settlerFlag && creep.room != Game.flags.settlerFlag.room)
             {
-                if(creep.room == Game.flags.settlerFlag.room)
-                {
-                    let targets = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
-                    let source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+                console.log('settler(' + creep.name + ') using fallback movement...')
+                    creep.moveTo(Game.flags.settlerFlag);
+            }
+            else
+            {
+                let targets = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
+                let source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
 
-                    if(targets && source)
+                if(targets && source)
+                {
+                    if((_.sum(creep.carry)  == 0) || (_.sum(creep.carry) < creep.carryCapacity && creep.pos.inRangeTo(source, 1)))
                     {
-                        if((_.sum(creep.carry)  == 0) || (_.sum(creep.carry) < creep.carryCapacity && creep.pos.inRangeTo(source, 1)))
+                        if(creep.harvest(source) == ERR_NOT_IN_RANGE) 
                         {
-                            if(creep.harvest(source) == ERR_NOT_IN_RANGE) 
-                            {
-                                creep.moveTo(source);
-                            }
-                        }  
-                        else
+                            creep.moveTo(source);
+                        }
+                    }  
+                    else
+                    {
+                        if(targets.length > 0) 
                         {
-                            if(targets.length > 0) 
+                            if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) 
                             {
-                                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) 
-                                {
-                                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffff00'}});
-                                }
+                                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffff00'}});
                             }
                         }
                     }
-                }
-                else
-                {
-                    console.log('settler(' + creep.name + ') using fallback movement...')
-                    creep.moveTo(Game.flags.settlerFlag);
                 }
             }
         }
