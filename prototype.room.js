992d8b8e66cb22c952309da6a6ec3ab0414c30ae
Jeremy Sletten <jsletten@enprecis.local>
2019-05-21 12:52:44 -0400

    Prioritize transport spawning

--- prototype.room.js
+++ prototype.room.js
@@ -291,7 +291,7 @@ Room.prototype.spawnResourceCreeps =
                 {
                     if((source.container.transports.length + this.spawnQueueCount('containerTransport')) < 1)
                     {
-                        this.addToSpawnQueue({role: 'containerTransport', targetID: source.container.id, homeRoom: this.name});
+                        this.addToSpawnQueue({role: 'containerTransport', targetID: source.container.id, homeRoom: this.name}, true);
                     }
                 }
                 else
