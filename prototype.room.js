a5845ead594674e152ef6e4abf5113306b8f2fd7
Jeremy Sletten <jsletten@enprecis.local>
2019-06-12 15:57:20 -0400

    susplend remote room healer to improve economy

--- prototype.room.js
+++ prototype.room.js
@@ -461,7 +461,8 @@ Room.prototype.spawnRemoteCreeps =
                     }
                     if((Globals.creepCountByRole('healer', remoteRoomId) + this.spawnQueueCount('healer')) < 1)
                     {
-                        this.addToSpawnQueue({role: 'healer', targetRoom: remoteRoomId});
+                        //Suspend healer for now to improve economy.  Rethink this as a single QRF that can respond to enemies in multiple remote rooms.
+                        //this.addToSpawnQueue({role: 'healer', targetRoom: remoteRoomId});
                     }
 
                     //fixer
