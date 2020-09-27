fb7edfe76b54ffb0e89b0501234108faa26385ca
Jeremy Sletten <jsletten@gmail.com>
2020-09-26 23:07:39 -0400

    remove transports from queue priority

--- prototype.room.js
+++ prototype.room.js
@@ -277,7 +277,7 @@ Room.prototype.spawnResourceCreeps =
             {
                 if((source.container.transports.length + this.spawnQueueCount('containerTransport')) < 1)
                 {
-                    this.addToSpawnQueue({role: 'containerTransport', targetID: source.container.id, homeRoom: this.name}, true);
+                    this.addToSpawnQueue({role: 'containerTransport', targetID: source.container.id, homeRoom: this.name});
                 }
             }
 
