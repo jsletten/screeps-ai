852797d3a67a42a52f10a9e4bdcfcdf2892c6ded
Jeremy Sletten <jsletten@gmail.com>
2020-09-29 12:19:34 -0400

    WIP generic BaseManager

---
+++ role.baseManager.js
@@ -0,0 +1,120 @@
+module.exports = {
+    buildBody: function(maxEnergy) 
+    {
+        maxEnergy = Math.min(maxEnergy, 1500);
+        let body = [];
+        let numberOfParts = Math.floor(maxEnergy / 150) * 3;
+
+        //1 MOVE part for every 2 CARRY parts
+        for (let i = 0; i < ((numberOfParts/3)*2); i++) {
+            body.push(CARRY);
+        }
+        for (let i = 0; i < (numberOfParts/3); i++) {
+            body.push(MOVE);
+        }
+        return body;
+    },
+    
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if(creep.store.getUsedCapacity() == 0) 
+        {
+            if(creep.room.name == creep.memory.homeRoom && creep.ticksToLive < 100)
+            {
+                creep.suicide();
+            }
+
+            let target = Game.getObjectById(creep.memory.targetID);
+                 
+            if(target && _.sum(target.store) < 100)
+            {
+                //Act as Cleaner
+                target = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
+
+                if(target) {
+                    creep.say('Resource!');
+                    if(creep.pickup(target) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    }
+                }
+                else 
+                {
+                    target = creep.pos.findClosestByPath(FIND_TOMBSTONES, {filter: (tombstone) => { 
+                        return (_.sum(tombstone.store) > 0)}}); 
+                
+                    if(target)
+                    {
+                        for(resourceType in target.store) 
+                        {
+                            if(creep.withdraw(target, resourceType) == ERR_NOT_IN_RANGE) {
+                                creep.moveTo(target);
+                            }
+                        }
+                    }
+                }
+            }
+            else if(target)
+            {
+                //Get resources from container
+                for(resourceType in target.store) 
+                {
+                    if(creep.withdraw(target, resourceType) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target, {reusePath: 15});
+                    }
+                }
+            }
+        }
+        else {
+            if(creep.room.name != creep.memory.homeRoom)
+            {
+                // find exit to target room
+                let exit = creep.room.findExitTo(creep.memory.homeRoom);
+                // move to exit
+                creep.moveTo(creep.pos.findClosestByRange(exit), {reusePath: 15});
+            }
+            else
+            {
+                let target;
+                
+                if(_.sum(creep.carry) == creep.carry[RESOURCE_ENERGY]) //Only energy so deposit anywhere
+                {
+                    target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => { 
+                        return ((structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_LINK) && ((structure.energy < structure.energyCapacity)&&(structure.energy < 800))
+                            || (structure.structureType == STRUCTURE_STORAGE))}});
+
+                    if(!target)
+                    {
+                        target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => { 
+                            return ((structure.structureType == STRUCTURE_TOWER) && ((structure.energy < structure.energyCapacity)&&(structure.energy < 800)))}}); 
+                    }
+
+                    if(!target)
+                    {
+                        let results = creep.room.spawns[0].pos.findInRange(FIND_STRUCTURES, 2, {filter: (structure) => { 
+                            return (structure.structureType == STRUCTURE_CONTAINER) && (_.sum(structure.store) < structure.storeCapacity)}});
+
+                        if(results.length > 0)
+                        {
+                            target = results[0];
+                        }    
+                    }
+                }
+                else
+                {
+                    target = creep.room.storage; //Carrying resources other then energy so must deposit to storage
+                }
+            
+                if(target)
+                {
+                    for(resourceType in creep.carry) 
+                    {
+                        if(creep.transfer(target, resourceType) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(target);
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
