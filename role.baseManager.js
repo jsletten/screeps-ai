47396536d2d103f7c7ecab383852b3f95c2e2acf
Jeremy Sletten <jsletten@gmail.com>
2020-09-29 20:48:48 -0400

    baseManager debugging

--- role.baseManager.js
+++ role.baseManager.js
@@ -33,6 +33,7 @@ module.exports = {
             else
             {
                 //Gather Energy
+                console.log('Gather Energy');
                 //Mining Container (closest over 100 seems to work could sort by fullest)
                 let sourceContainers = [];
                 for(let sourceIndex in this.sources)
@@ -48,12 +49,14 @@ module.exports = {
                 if(sourceContainers.length > 0)
                 {
                     target = creep.pos.findClosestByPath(sourceContainers);
+                    console.log('Gather Energy - sourceContainer');
                 }
 
                 //Storage
                 if(!target && creep.room.storage && creep.room.storage.store[RESOURCE_ENERGY] > 0)
                 {
                     target = creep.room.storage;
+                    console.log('Gather Energy - Storage');
                 }
 
                 //Spawn Containers
@@ -65,11 +68,13 @@ module.exports = {
                     if(results.length > 0)
                     {
                         target = creep.pos.findClosestByPath(results);
+                        console.log('Gather Energy - Spawn Containers');
                     }  
                 }
 
                 if(target)
                 {
+                    console.log('Gather Energy - Target Found');
                     if(creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) 
                     {
                         creep.moveTo(target, {reusePath: 10});
@@ -80,12 +85,13 @@ module.exports = {
         else
         {
             //Deliver Energy
-
+            console.log('Deliver Energy');
             //Extensions
             if(!target)
             {
                 target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => { 
                     return ((structure.structureType == STRUCTURE_EXTENSION) && (structure.store[RESOURCE_ENERGY] < structure.store.getCapacity()))}}); 
+                console.log('Deliver Energy - Extensions');
             }
             
             //Spawns
@@ -93,6 +99,7 @@ module.exports = {
             {
                 target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => { 
                     return ((structure.structureType == STRUCTURE_SPAWN) && (structure.store[RESOURCE_ENERGY] < structure.store.getCapacity()))}}); 
+                console.log('Deliver Energy - Spawns');
             }
 
             //Controller Container under 50%
@@ -102,6 +109,7 @@ module.exports = {
                 if(roomController && roomController.container && !roomController.link && roomController.container.store[RESOURCE_ENERGY] < roomController.container.getCapacity()/2)
                 {
                     target = roomController.container;
+                    console.log('Deliver Energy - Controller Container');
                 }
             }
 
@@ -110,6 +118,7 @@ module.exports = {
             {
                 target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (structure) => { 
                     return ((structure.structureType == STRUCTURE_TOWER) && (structure.store[RESOURCE_ENERGY] < structure.store.getCapacity()/2))}}); 
+                console.log('Deliver Energy - Tower');
             }
 
             //Spawn Containers
@@ -121,6 +130,7 @@ module.exports = {
                 if(results.length > 0)
                 {
                     target = creep.pos.findClosestByPath(results);
+                    console.log('Deliver Energy - Spawn Countainer');
                 }    
             }
 
@@ -128,6 +138,7 @@ module.exports = {
             if(!target && creep.room.storage && creep.room.storage.store[RESOURCE_ENERGY] < structure.store.getCapacity(RESOURCE_ENERGY)/2)
             {
                 target = creep.room.storage;
+                console.log('Deliver Energy - Storage');
             }
 
             if(target)
@@ -135,13 +146,16 @@ module.exports = {
                 if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
                 }
+                console.log('Deliver Energy - Target Found');
             }
             else
             {
+                console.log('Deliver Energy - No Target');
                 //Couldn't find a delivery target...
                 //Gather if not full or HOLD it till someone needs it
                 if(creep.store.getFreeCapacity() > 0)
                 {
+                    console.log('Deliver Energy - Try to gather');
                     creep.memory.gather = true;
                 }
             }
