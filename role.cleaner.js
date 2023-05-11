d92a9d837741a2f652b82e250ede9440062536ab
jsletten <jsletten@gmail.com>
2023-05-11 13:03:58 -0400

    Cleaner always dump to storage.

--- role.cleaner.js
+++ role.cleaner.js
@@ -38,24 +38,11 @@ module.exports = {
                         }
                     }
                 }
-                else
-                {
-                    //TODO: Shouldn't hardcode waiting location
-                    creep.moveTo(24,24);
-                }
             }
         }
         else 
         {
-            let target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (structure) => { 
-                return ((structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER) && _.sum(structure.store) < structure.storeCapacity)}});            
-            
-            
-            if(!target)
-            {
-                target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: (structure) => { 
-                    return (structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_TOWER ) && ((structure.energy < structure.energyCapacity)&&(structure.energy < 800))}});                    
-            }
+            let target = creep.room.storage;
 
             if(target)
             {
@@ -66,13 +53,6 @@ module.exports = {
                     }
                 }
             }
-            // else
-            // {
-            //     for(const resourceType in creep.carry) 
-            //     {
-            //         creep.transfer(target, resourceType);
-            //     }
-            // }
         }
     }
 };
